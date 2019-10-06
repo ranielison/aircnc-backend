cdb59afb91b92c41109909d6cfe1f90c489d9fec
Ranielison <ranielisonsoares2@gmail.com>
2019-10-06 13:14:17 -0300

    ajustando para pegar arquivos de imagens

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
+const path = require("path");
 const routes = require("./routes");
-
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/aircnc", {
@@ -10,7 +10,9 @@ mongoose.connect("mongodb://localhost:27017/aircnc", {
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
